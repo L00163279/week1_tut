3b76ad8ac11fcb2a51e75eff8224e5f7ef2ead2a
MShafeeq <L00163279@student.lyit.ie>
2021-09-27 07:40:26 +0100

    Third commit

--- q2.py
+++ q2.py
@@ -5,3 +5,17 @@ left_icon="<"
 right_icon=">"
 
 print("Title")
+
+print(top_icon*48)
+print(left_icon + "\t" * 12+right_icon)
+print(left_icon + "\t" * 12+right_icon)
+print(left_icon + "\t" * 12+right_icon)
+print(left_icon + "\t" * 5 + "Title" + "\t" * 6 +right_icon)
+print(left_icon + "\t" * 12+right_icon)
+print(left_icon + "\t" * 12+right_icon)
+print(left_icon + "\t" + "A \t option a" + "\t" * 8 + right_icon)
+print(left_icon + "\t" + "B \t option b" + "\t" * 8 + right_icon)
+print(left_icon + "\t" + "C \t option c" + "\t" * 8 + right_icon)
+print(left_icon + "\t" * 12+right_icon)
+print(left_icon + "\t" * 12+right_icon)
+print(bottom_icon*48)
